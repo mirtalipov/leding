8eb631c81c0ca6c3a80c12bfb03b0422ed171c6a
javohir <javohirmirtalipov03@gmail.com>
2024-10-24 00:59:58 +0500

    добавление web.php файла

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\WorkerController;
+
 route::get('/', function () {
     return view('welcome');
 });
 
 
-route::get('/workers', function(){
-    return 'this is worker from routes';
-});
+route::get('/workers', [WorkerController::class, 'index']); 
+route::get('/show', [WorkerController::class, 'show']);
